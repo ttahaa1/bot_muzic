0c6ae603028df530d06b14e847470fff8d7bca8a
speed <153678863+ttahaa1@users.noreply.github.com>
2024-04-04 03:46:53 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://i.pinimg.com/originals/a9/0e/54/a90e54ebea40010708f8d8a9c27eaae1.jpg")
-START_IMG = getenv("START_IMG", "https://i.pinimg.com/originals/a9/0e/54/a90e54ebea40010708f8d8a9c27eaae1.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/3e5039ee25df9814417c8.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/3e5039ee25df9814417c8.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/b1o_d_a")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6264668799").split()))
 
 
-FAILED = "https://i.pinimg.com/originals/a9/0e/54/a90e54ebea40010708f8d8a9c27eaae1.jpg"
+FAILED ="https://telegra.ph/file/3e5039ee25df9814417c8.jpg"
